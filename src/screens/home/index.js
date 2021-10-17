4c3afacb6625241dd602042b13be991e19997def
Arnold Ngass <ngassamarnold@gmail.com>
2021-10-17 12:11:36 +0100

    Add navigate to Home

---
+++ src/screens/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import colors from '@constants/colors';
+import { Center, Content, ContentButton, ContentVariable, DivFlex, Text, TextTitle } from '@components/utils';
+import CreditCard from '@components/Circle';
+import Button from '@components/buttons';
+
+export default function Welcome({ navigation }) {
+
+
+    return (
+        <Content >
+
+        </Content>
+    );
+}
