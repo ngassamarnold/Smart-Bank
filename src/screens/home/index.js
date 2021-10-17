ebd2be1c8ca18c4cb246cc9c38bd076e8ccca36f
Arnold Ngass <ngassamarnold@gmail.com>
2021-10-17 13:52:30 +0100

    Add section settings in home

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,7 +1,7 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import colors from '@constants/colors';
-import { BR, Center, Content, ContentButton, ContentVariable, DivFlex, Text, TextTitle } from '@components/utils';
+import { BR, Center, Content, ContentButton, ContentVariable, DivFlex, FlexDirection, Row, Text, TextTitle } from '@components/utils';
 import CreditCard from '@components/CreditCard';
 import Button from '@components/buttons';
 import { SwitchInput } from '@components/switchInput';
@@ -20,27 +20,57 @@ export default function Welcome({ navigation }) {
                     bgColor={colors.yellow}
                     name="NGASSAM F. Arnold"
                     date="22/05/2022"
-                    suffix="2200" 
-                    type="Virtual Card"/>
+                    suffix="2200"
+                    type="Virtual Card" />
                 <BR val="2" />
                 <CreditCard
                     bgColor={colors.black}
                     name="NGASSAM F. Arnold"
                     date="22/05/2022"
-                    suffix="2200" 
-                    type="Physical Card"/>
+                    suffix="2200"
+                    type="Physical Card" />
             </Center>
             <BR val="2" />
             <TextTitle position="left" left={15} size={15} color={colors.black} > Card settings </TextTitle>
             <BR />
-            <SwitchInput
-                isEnabled={isEnabled} 
-                toggleSwitch={toggleSwitch} />
+            <ItemsSetting>
+                <Row>
+                    <DivFlex flex={4}>
+                        <TextTitle position="left" left={35} size={15} color={colors.black} > Contactless payment </TextTitle>
+                    </DivFlex>
+                    <DivFlex jc="flex-end">
+                        <SwitchInput
+                            isEnabled={!isEnabled}
+                            toggleSwitch={toggleSwitch} />
+                    </DivFlex>
+                </Row>
+                <BR />
+                <Row>
+                    <DivFlex flex={4}>
+                        <TextTitle position="left" left={35} size={15} color={colors.black} > Online payment </TextTitle>
+                    </DivFlex>
+                    <DivFlex jc="flex-end">
+                        <SwitchInput
+                            isEnabled={!isEnabled}
+                            toggleSwitch={toggleSwitch} />
+                    </DivFlex>
+                </Row>
+                <BR />
+                <Row>
+                    <DivFlex flex={4}>
+                        <TextTitle position="left" left={35} size={15} color={colors.black} > ATM Withdrawals </TextTitle>
+                    </DivFlex>
+                    <DivFlex jc="flex-end">
+                        <SwitchInput
+                            isEnabled={isEnabled}
+                            toggleSwitch={toggleSwitch} />
+                    </DivFlex>
+                </Row>
+            </ItemsSetting>
         </Content>
     );
 }
 
-const TestDescription = styled(TextTitle)`
-  line-height: 25px;
+const ItemsSetting = styled.View`
+    width: 95%;
 `;
-
