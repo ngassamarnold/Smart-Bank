34be535cb5d08a56042f51774f1445032466d226
Arnold Ngass <ngassamarnold@gmail.com>
2021-10-17 13:24:51 +0100

    Add component Switch

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState} from 'react';
 import styled from 'styled-components';
 import colors from '@constants/colors';
 import { BR, Center, Content, ContentButton, ContentVariable, DivFlex, Text, TextTitle } from '@components/utils';
 import CreditCard from '@components/CreditCard';
 import Button from '@components/buttons';
+import { SwitchInput } from '@components/switchInput';
 
 export default function Welcome({ navigation }) {
-
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
     return (
         <Content jc="flex-start">
@@ -31,6 +33,9 @@ export default function Welcome({ navigation }) {
             <BR val="2" />
             <TextTitle position="left" left={15} size={15} color={colors.black} > Card settings </TextTitle>
             <BR />
+            <SwitchInput
+                isEnabled={isEnabled} 
+                toggleSwitch={toggleSwitch} />
         </Content>
     );
 }
